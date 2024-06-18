08d6646ea73a937eda22efcee9b3410d23487bc4
MarvinRF <mamavivi@gmail.com>
2024-06-17 23:52:47 -0300

    features and how it works done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
 import Model from "./components/Model";
 import * as Sentry from "@sentry/react";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
+      <HowItWorks />
     </main>
   );
 };
--- src/utils/animations.js
+++ src/utils/animations.js
@@ -3,6 +3,18 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
 
+export const animateWithGsap = (target, animationProps, scrollProps) => {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: "restart reverse restart reverse",
+      start: "top 85%",
+      ...scrollProps,
+    },
+  });
+};
+
 export const animateWithGsapTimeline = (
   timeline,
   rotationRef,
